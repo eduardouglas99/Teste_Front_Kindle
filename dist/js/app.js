6ce0cdb9bbadccb8e96901827a6227c2cf30495d
Eduardo Douglas de Melo Caetano <ecaetano@marlin.net>
2021-03-04 21:11:31 -0300

    ajustes na sessao como funciona, calcule e vantagens layout finalizado v desk

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
     
+    $('.link-scroll').click(function(){
+        var href = $(this).attr('href');
+        var offSetTop = $(href).offset().top;
+    
+    $('html,body').animate({'scrollTop':offSetTop}, 1200);
+        return false;
+    });
+
+    // Mascara calcule sua economia
+    $('#valor').mask('#.##0,00', {reverse: true});
+    $('#telefone').mask('(000)00000-0000');
 });
--- index.php
+++ index.php
@@ -7,32 +7,12 @@
     <link rel="icon" type="image/png" href="dist/img/ms-icon-150x150.png">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <link rel="stylesheet" type="text/css" href="dist/css/app.css?v=20210118v1">
-    <!-- <script type="text/javascript" src="dist/lib/jquery/jquery-3.5.1.min.js"></script>
-    <script type="text/javascript" src="dist/lib/jquery/jquery-migrate-3.3.2.js"></script> -->
-    <!-- <script type="text/javascript" src="dist/lib/slick/slick.min.js"></script> -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script type="text/javascript" src="dist/lib/jquery.mask.js"></script>
-    <!-- <link rel="stylesheet" type="text/css" href="dist/lib/slick/slick.css" /> -->
     <script type="text/javascript" src="dist/js/app.js?v=20210118v1"></script>
     <!-- <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"> -->
-    <!-- <link href="dist/lib/fontawesome-free-5.14.0-web/css/all.css" rel="stylesheet"> -->
     <script src="https://kit.fontawesome.com/f9040905d9.js" crossorigin="anonymous"></script>
     <!-- <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script> -->
-    <!-- <meta name='description'
-        content='São inúmeras as empresas parceiras que oferecem acesso a descontos e benefícios em diversas categorias, 
-        como farmácias, educação, eletrodomésticos, alimentos, hotéis, móveis e decoração, pet shop, magazine, perfumaria etc.' />
-    <meta name='keywords'
-        content='eNOSS, eNOSS Benefícios, eNOSS Descontos, NOSS Vantagens, eNOSS Descontos e Vnatagens' />
-    <meta property="og:image" content="https://grminternet.com.br/hmg/enoss-beneficios-corretores/dist/img/enoss-lp-beneficios-ogimage.png">
-    <meta property="og:title" content="eNOSS - Benefícios">
-    <meta property="og:description"
-        content="São inúmeras as empresas parceiras que oferecem acesso a descontos e benefícios em diversas categorias, 
-        como farmácias, educação, eletrodomésticos, alimentos, hotéis, móveis e decoração, pet shop, magazine, perfumaria etc.">
-    <meta property="og:url" content="https://grminternet.com.br/hmg/enoss-beneficios-corretores/">
-    <meta property="og:type" content="article">
-    <meta property="og:locale" content="pt-br">
-    <meta property="og:site_name" content="eNOSS">
-    <meta name="theme-color" content="#efb700" /> -->
 </head>
 
 <body>
@@ -46,16 +26,16 @@
             </div>
             <div class="Header_Site_Menu">
                 <ul class="flex">
-                    <a href="#">
+                    <a href="#ComoFunciona" class="link-scroll">
                         <li>Como functiona</li>
                     </a>
-                    <a href="#">
+                    <a href="#Section_Vantagens" class="link-scroll">
                         <li>Vantagens & Sustentabilidade</li>
                     </a>
-                    <a href="#">
+                    <a href="#CalculeSuaEconomia" class="link-scroll">
                         <li>Calcule sua economia</li>
                     </a>
-                    <a href="#">
+                    <a href="#Faq" class="link-scroll">
                         <li>FAQ</li>
                     </a>
                 </ul>
@@ -66,165 +46,93 @@
                 </a>
             </div>
         </div>
-        <section class="Saiba_Mais">
+        <section class="Saiba_Mais" id="ComoFunciona">
             <div class="container">
-                <h5>Eva energia</h5>
-                <h3>Economize na sua conta de luz e ajude o planeta sem investir nada</h3>
+                <h5 class="subtitle">Eva energia</h5>
+                <h3 class="title">Economize na sua conta de luz e ajude o planeta sem investir nada</h3>
                 <div class="Saiba_Mais_Content flex">
                     <button class="Btn_Default">Quero saber mais</button>
-                    <a href="#">Calcular desconto</a>
+                    <a href="#CalculeSuaEconomia" class="link-scroll">Calcular desconto</a>
                     <img src="dist/img/Estrela_Eva" alt="">
                 </div>
                 <div class="Como_Funciona">
                     <div class="Como_Funciona_Content">
-                        <h5>Como funciona</h5>
-                        <h3>Veja tudo o que você precisa saber para tomar a decisão mais fácil da sua vida para sua empresa</h3>
+                        <h5 class="subtitle">Como funciona</h5>
+                        <h3 class="title">Veja tudo o que você precisa saber para tomar a decisão mais fácil da sua vida para sua empresa</h3>
                         <img src="dist/img/Estrela_Eva" alt="">
                     </div>
                     <div class="Como_Funciona_Video">
-                        <img src="dist/img/Video.png" alt="">
+                        <img src="dist/img/Btn_Play.png" alt="">
+                        <img src="dist/img/Nuvens_Video" alt="">
+                        <!-- <img src="dist/img/Ilustra_Farm" alt=""> -->
                     </div>
                 </div>
+            </div>
+            <div class="Banner_Fazenda">
+                <img src="dist/img/Grupo_Farm.png" alt="">
             </div>
         </section>
     </header>
-    <!-- <section class="Section_Beneficios">
+    <section class="Section_CalculeSuaEconomia" id="CalculeSuaEconomia">
+        <div class="Bg_Color"></div>
         <div class="container">
-            <div class="Section_Beneficios_Title">
-                <h2>Benefícios</h2>
-                <h5>Descontos e Vantagens</h5>
-            </div>
-            <div class="Section_Group flex">
-                <div class="Section_Beneficios_Content">
-                    <p>São inúmeras as empresas parceiras que oferecem acesso a descontos e benefícios em diversas
-                        categorias, como farmácias,
-                        educação, eletrodomésticos, alimentos, hotéis, móveis e decoração, pet shop, magazine,
-                        perfumaria etc. Abaixo alguns
-                        desses parceiros:
-                    </p>
-                    <div class="Section_Beneficios_Parceiros grid">
-                        <a href="https://www.aramis.com.br" target="_blank">
-                            <img src="dist/img/parceiros/1-aramis-menswear.png" alt="Aramis Menswear"
-                                title="Aramis Menswear">
-                        </a>
-                        <a href="https:/www.canon.com.br" target="_blank">
-                            <img src="dist/img/parceiros/2-canon.png" alt="Canon" title="Canon">
-                        </a>
-                        <a href="https:/www.centauro.com.br" target="_blank">
-                            <img src="dist/img/parceiros/3-centauro.png" alt="Centauro" title="Centauro">
-                        </a>
-                        <a href="https:/www.giulianaflores.com.br/" target="_blank">
-                            <img src="dist/img/parceiros/4-giulianaflores.png" alt="Giuliana Flores"
-                                title="Giuliana Flores">
-                        </a>
-                        <a href="https://cnago.com.br/" target="_blank">
-                            <img src="dist/img/parceiros/5-cna-go.png" alt="CNA Go" title="CNA Go">
-                        </a>
-                        <a href="https://www.compracerta.com.br/" target="_blank">
-                            <img src="dist/img/parceiros/6-compra-certa-descontos.png" alt="Compra Certa"
-                                title="Compra Certa">
-                        </a>
-                        <a href="https://www.dafiti.com.br/" target="_blank">
-                            <img src="dist/img/parceiros/7-dafiti-2048.png" alt="Dafiti" title="Dafiti">
-                        </a>
-                        <a href="https://emporionestle.com.br/produtos/emporio-nestle-nacional/" target="_blank">
-                            <img src="dist/img/parceiros/8-nestle.png" alt="Nestle" title="Nestle">
-                        </a>
-                        <a href="https://www.evino.com.br/" target="_blank">
-                            <img src="dist/img/parceiros/9-Evino.png" alt="Evino" title="Evino">
-                        </a>
-                        <a href="https://www.hering.com.br/store/" target="_blank">
-                            <img src="dist/img/parceiros/10-hering.png" alt="Hering" title="Hering">
-                        </a>
-                        <a href="https://www.hoteis.com/?intlid=HOME+%3A%3A+header_main_section" target="_blank">
-                            <img src="dist/img/parceiros/11-hoteiscom.png" alt="Hotéis.com" title="Hotéis.com">
-                        </a>
-                        <a href="https://br.loccitane.com/" target="_blank">
-                            <img src="dist/img/parceiros/12-LOccitane-.png" alt="Loccitane" title="Loccitane">
-                        </a>
-                        <a href="https://www.americanas.com.br/" target="_blank">
-                            <img src="dist/img/parceiros/13-americanas-.png" alt="Americanas" title="Americanas">
-                        </a>
-                        <a href="https://www.webfones.com.br/" target="_blank">
-                            <img src="dist/img/parceiros/14-webfones-.png" alt="Webfones" title="Webfones">
-                        </a>
-                        <a href="https://www.lojasrenner.com.br/" target="_blank">
-                            <img src="dist/img/parceiros/15-renner-logo-1-1.png" alt="Renner" title="Renner">
-                        </a>
-                        <a href="https://www.sephora.com.br/" target="_blank">
-                            <img src="dist/img/parceiros/16-Sephora-logo.png" alt="Sephora" title="Sephora">
-                        </a>
-                        <a href="https://www.marabraz.com.br/" target="_blank">
-                            <img src="dist/img/parceiros/17-marabraz.png" alt="Marabraz" title="Marabraz">
-                        </a>
-                        <a href="https://www.loja.philips.com.br/" target="_blank">
-                            <img src="dist/img/parceiros/18-philips.png" alt="Philips" title="Philips">
-                        </a>
-                        <a href="https://www.puket.com.br/" target="_blank">
-                            <img src="dist/img/parceiros/19-puket.png" alt="Puket" title="Puket">
-                        </a>
-                        <a href="https://www.shopclub.com.br/" target="_blank">
-                            <img src="dist/img/parceiros/20-shopclub.png" alt="Shopclub" title="Shopclub">
-                        </a>
-                        <a href="https://www.tokstok.com.br/" target="_blank">
-                            <img src="dist/img/parceiros/21-tokstok.png" alt="Tok & Stok" title="Tok & Stok">
-                        </a>
-                        <a href="" target="_blank">
-                            <img src="dist/img/parceiros/22-k.png" alt="K" title="K">
-                        </a>
-                    </div>
+            <h5 class="subtitle">Calcule a sua economia</h5>
+            <h3 class="title">Preencha os dados e descubra o quanto sua empresa consegue economizar</h3>
+            <form class="Section_CalculeSuaEconomia_Form flex">
+                <div class="Section_CalculeSuaEconomia_Form_Campos flex Campo_UltimaConta">
+                    <label for="valor" class="subtitle">Última conta (R$)</label>
+                    <input type="text" id="valor" required>
                 </div>
-                
-            </div>
-        </div>
-        <div class="Fale_Conosco">
-            <div class="Fale_Conosco_Box">
-                <div class="Fale_Conosco_Title flex">
-                    <h3>Contato</h3>
-                    <h4>Ficou interessado?</h4>
-                    <p>Fale conosco através dos nossos <span>canais online</span> ou via
-                        <span>Whatsapp</span>!
-                    </p>
+                <div class="Section_CalculeSuaEconomia_Form_Campos flex Campo_Nome">
+                    <label for="nome" class="subtitle">Nome</label>
+                    <input type="text" required>
                 </div>
-                <div class="Fale_Conosco_Logotipos_Item flex">
-                    <a href="https://www.facebook.com/enossplataforma">
-                        <i class="fab fa-facebook-square facebook"></i>
-                    </a>
-                    <a href="https://www.instagram.com/enossplataforma/">
-                        <i class="fab fa-instagram-square"></i>
-                    </a>
-                    <a href="https://www.linkedin.com/company/enoss">
-                        <i class="fab fa-linkedin"></i>
-                    </a>
-                    <a href="https://www.youtube.com/channel/UCVTkiNja5DeBrsmXYM8xAFA">
-                        <i class="fab fa-youtube youtube"></i>
-                    </a>
-                    <a href="https://www.youtube.com/channel/UCVTkiNja5DeBrsmXYM8xAFA">
-                        <i class="fab fa-whatsapp-square"></i>
-                    </a>
-                </div> -->
-                <!-- <div class="Fale_Conosco_Logotipos_Item_Btn">
-                    <a href="https://api.whatsapp.com/send?phone=5519999999999&text=Entre em contato conosco!"
-                        target="_blank" class="flex">
-                        <i class="fab fa-whatsapp whatsapp"></i>
-                        <P>Whatsapp</P>
-                    </a>
-                </div> -->
-            <!-- </div>
+                <div class="Section_CalculeSuaEconomia_Form_Campos flex Campo_Email">
+                    <label for="email" class="subtitle">Email</label>
+                    <input type="email" required>
+                </div>
+                <div class="Section_CalculeSuaEconomia_Form_Campos flex Campo_Telefone">
+                    <label for="telefone" class="subtitle">Telefone</label>
+                    <input type="text" id="telefone" required>
+                </div>
+                <button class="Btn_Default">Calcular Desconto</button>
+            </form>
         </div>
-    </section> -->
-
-
-    <!-- <footer class="Site_Footer">
-        <div class="container flex">
-            <div class="Site_Footer_DeveloperBy flex">
-                <a href="https://marlin.com.br/" target="_blank">
-                    <p> Desenvolvido por</p>
-                    <img src="dist/img/LogoMarlin.png">
-                </a>
+    </section>
+    <section class="Section_Vantagens" id="Section_Vantagens">
+        <div class="container">
+            <h5 class="subtitle">Bom para todo mundo</h5>
+            <h3 class="title">Vantagens & Sustentabilidade</h3>
+            <div class="Section_Vantagens_Box grid">
+                <div class="Section_Vantagens_Box_Item flex">
+                    <p>Consume energia limpa e renovável</p>
+                    <img src="dist/img/Grid-Item-1.png" alt="">
+                </div>
+                <div class="Section_Vantagens_Box_Item flex">
+                    <p>Economize, pelo menos, 1 conta de luz ao ano</p>
+                    <img src="dist/img/Grid-Item-2.png" alt="">
+                </div>
+                <div class="Section_Vantagens_Box_Item flex">
+                    <p>Não precisa fazer obra e nenhum tipo de instalação</p>
+                    <img src="dist/img/Grid-Item-3.png" alt="">
+                </div>
+                <div class="Section_Vantagens_Box_Item flex">
+                    <p>Não é necessário fazer nenhum tipo de investimento</p>
+                    <img src="dist/img/Grid-Item-4.png" alt="">
+                </div>
+                <div class="Section_Vantagens_Box_Item flex">
+                    <p>Você faz a sua parte para cuidar do meio ambiente</p>
+                    <img src="dist/img/Grid-Item-5.png" alt="">
+                </div>
+                <div class="Section_Vantagens_Box_Item flex">
+                    <p>O desconto já começa no momento em que você assina o contrato</p>
+                    <img src="dist/img/Grid-Item-6.png" alt="">
+                </div>
             </div>
+            <img src="dist/img/Ilustra_Predio.png" alt="">
         </div>
-    </footer> -->
+    </section>
+
 </body>
 
 </html>
